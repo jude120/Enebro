40c06fc2fa20aba6449156477a9abab6dfc59361
Emmanuel2424 <ing.emmanuelsalgado@gmail.com>
2019-11-13 20:23:05 -0600

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola laravel
+                    Hola
                 </div>
 
                 <div class="links">
